8d591fc2aa7deed2a2c53e8b391b77d840b0678c
Bunchhieng <Bunchhieng@gmail.com>
2016-09-27 19:11:05 -0400

    clean up

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,47 @@
-var gulp         = require('gulp')
-var path         = require('path')
-var less         = require('gulp-less')
+var gulp = require('gulp')
+var path = require('path')
+var less = require('gulp-less')
 var autoprefixer = require('gulp-autoprefixer')
-var sourcemaps   = require('gulp-sourcemaps')
-var minifyCSS    = require('gulp-minify-css')
-var rename       = require('gulp-rename')
-var concat       = require('gulp-concat')
-var uglify       = require('gulp-uglify')
-var connect      = require('gulp-connect')
-var open         = require('gulp-open')
+var sourcemaps = require('gulp-sourcemaps')
+var minifyCSS = require('gulp-minify-css')
+var rename = require('gulp-rename')
+var concat = require('gulp-concat')
+var uglify = require('gulp-uglify')
+var connect = require('gulp-connect')
+var open = require('gulp-open')
 
 var Paths = {
-  HERE                 : './',
-  DIST                 : 'dist',
-  DIST_TOOLKIT_JS      : 'dist/toolkit.js',
-  LESS_TOOLKIT_SOURCES : './less/toolkit*',
-  LESS                 : './less/**/**',
-  JS                   : [
-      './js/jquery.min.js',
-      './js/odometer.min.js',
-      './js/highlight.pack.js',
-      './js/bootstrap.min.js',
-      './js/jqBootstrapValidation.js',
-      './js/d3.v4.min.js',
-      //'./js/jquery.fullpage.min.js',
-      './js/typed.js',
-      './js/custom/*',
-      './js/application.js',
-    ]
+  HERE: './',
+  DIST: 'dist',
+  DIST_TOOLKIT_JS: 'dist/toolkit.js',
+  LESS_TOOLKIT_SOURCES: './less/toolkit*',
+  LESS: './less/**/**',
+  JS: [
+    './js/jquery.min.js',
+    './js/odometer.min.js',
+    './js/highlight.pack.js',
+    './js/bootstrap.min.js',
+    './js/jqBootstrapValidation.js',
+    './js/d3.v4.min.js',
+    //'./js/jquery.fullpage.min.js',
+    './js/typed.js',
+    './js/custom/*',
+    './js/application.js',
+  ]
 }
 
 gulp.task('default', ['js-min', 'less-min', 'server', 'watch'])
 
 gulp.task('watch', function () {
-  gulp.watch(Paths.JS,   ['js-min']);
+  gulp.watch(Paths.JS, ['js-min']);
   gulp.watch(Paths.LESS, ['less-min']);
 })
 
 gulp.task('docs', ['server'], function () {
   gulp.src(__filename)
-    .pipe(open({uri: 'http://localhost:9000/docs/'}))
+    .pipe(open({
+      uri: 'http://localhost:9000/docs/'
+    }))
 })
 
 gulp.task('server', function () {
@@ -50,7 +52,9 @@ gulp.task('server', function () {
   });
 
   gulp.src(__filename)
-    .pipe(open({uri: 'http://localhost:9000/'}));
+    .pipe(open({
+      uri: 'http://localhost:9000/'
+    }));
 })
 
 gulp.task('less', function () {
@@ -76,7 +80,7 @@ gulp.task('less-min', ['less'], function () {
 })
 
 gulp.task('js', function () {
-  console.log("Running js!"); 
+  console.log("Running js!");
   return gulp.src(Paths.JS)
     .pipe(concat('toolkit.js'))
     .pipe(gulp.dest(Paths.DIST))
@@ -90,4 +94,3 @@ gulp.task('js-min', ['js'], function () {
     }))
     .pipe(gulp.dest(Paths.DIST))
 })
-
